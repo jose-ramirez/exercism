fd9c26f7d2e2ee2c621fb354edef29ef9e362158
Jose Ramirez <a30673083@gmail.com>
2016-08-25 15:20:01 -0300

    Nucleotide count exercise.
    Seems kinda complicated, but, it works.
    
    Also, it could be simplified using merge in the nucleotide-counts]
    function instead of calling frequencies and later fill in the missing
    letters in the final count.

---
+++ clojure/nucleotide-count/project.clj
@@ -0,0 +1,6 @@
+(defproject nucleotide-count "0.1.0-SNAPSHOT"
+  :description "nucleotide-count exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/nucleotide-count"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins[[lein-midje "3.0.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
