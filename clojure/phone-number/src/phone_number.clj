c19c826176487ee01d42dc10aa26dda7d3b19613
Jose Ramirez <a30673083@gmail.com>
2016-08-25 15:33:30 -0300

    Phone number, first iteration.
    Nice one, pretty easy to do by parts, i.e., first tackle the number
    function, then move on to the area-code function, etc.

---
+++ clojure/phone-number/src/phone_number.clj
@@ -0,0 +1,37 @@
+(ns phone-number)
+
+(require '[clojure.string :as s])
+
+(defn filter-number [number_string]
+  (reduce
+    #(s/replace %1 %2 "")
+    number_string ["." "(" ")" "-" " "]))
+
+(defn beautify [n]
+  (format
+    "(%s) %s-%s"
+      (subs n 0 3)
+      (subs n 3 6)
+      (subs n 6 10)))
+
+(defn number [number_string]
+  (let
+    [wo-signs (filter-number number_string)]
+    (cond
+      (= 10 (count wo-signs)) wo-signs
+      (and (= 11 (count wo-signs)) (= \1 (first wo-signs))) (subs wo-signs 1)
+      :else "0000000000")))
+
+(defn area-code [number_string]
+  (let
+    [wo-signs (filter-number number_string)]
+    (cond
+      (= 10 (count wo-signs)) (subs wo-signs 0 3)
+      (and (= 11 (count wo-signs)) (= \1 (first wo-signs))) (subs wo-signs 1 4))))
+
+(defn pretty-print [number_string]
+  (let
+    [wo-signs (filter-number number_string)]
+    (cond
+      (= 10 (count wo-signs)) (beautify wo-signs)
+      (and (= 11 (count wo-signs)) (= \1 (first wo-signs))) (beautify (subs wo-signs 1)))))
