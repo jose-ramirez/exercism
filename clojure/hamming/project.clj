8dbed5b8972a76a9f7f05b38bffa796e3b5f208c
Jose Ramirez <a30673083@gmail.com>
2016-08-25 15:21:34 -0300

    Hamming distance.
    Tried to do it the hardest way possible, but in the end I realized it's
    preeeetty simple.
    
    This the first iteration though, since I received some comments about
    the code to improve it (yay!).

---
+++ clojure/hamming/project.clj
@@ -0,0 +1,6 @@
+(defproject hamming "0.1.0-SNAPSHOT"
+  :description "hamming exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/hamming"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-midje "3.0.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
