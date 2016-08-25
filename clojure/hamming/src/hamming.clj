af5fdbe4d6d9dec37235c0c87577a2dec2276779
Jose Ramirez <a30673083@gmail.com>
2016-08-25 15:23:58 -0300

    Hamming distance, second iteration.
    I removed the char-array call, since the code would work without them
    as well, and used the first and second functions in the reducing
    function.
    
    Looks a bit nicer now :)

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,9 +1,8 @@
 (ns hamming)
 
 (defn distance [c1 c2]
-  (if (not (= (count c1) (count c2)))
+  (if (not= (count c1) (count c2))
     nil
-    (reduce + (map
-      #(if (= (nth % 0) (nth % 1)) 0 1)
-      (map vector (char-array c1) (char-array c2))))
-  ))
+    (reduce +
+      (map #(if (= (first %) (second %)) 0 1)
+      (map vector c1 c2)))))
