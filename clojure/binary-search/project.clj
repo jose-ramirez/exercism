6a589863bb6ba186a98753d51c8dd86575c4e299
hpRamirez <a30673083@gmail.com>
2016-09-10 18:12:20 -0300

    Binary search algorithm.
    I cheated on this one, too lazy to implement (again) this algorithm in
    clojure.

---
+++ clojure/binary-search/project.clj
@@ -0,0 +1,10 @@
+(defproject binary-search "0.1.0-SNAPSHOT"
+  :description "binary-search exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/binary-search"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {
+    :dev {
+      :plugins [[lein-midje "3.2"]]
+      :dependencies [[midje "1.8.3"]]
+    }
+  })
