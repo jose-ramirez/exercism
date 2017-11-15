a8ab3b11a6cd49af8d8be978556abbb7f28bc418
Jose Ramirez <a30673083@gmail.com>
2017-11-15 14:41:44 -0200

    Acronym exercise, in Clojure.

---
+++ clojure/acronym/project.clj
@@ -0,0 +1,6 @@
+(defproject acronym "0.1.0-SNAPSHOT"
+  :description "acronym exercise."
+  :plugins [[lein-midje "3.2"]]
+  :url "https://github.com/exercism/clojure/tree/master/exercises/acronym"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
