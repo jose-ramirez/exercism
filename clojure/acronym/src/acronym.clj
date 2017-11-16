fd78f8802a3a96315c7cba732ff02e6e20dd40a7
Jose Ramirez <a30673083@gmail.com>
2017-11-16 08:16:35 -0200

    2nd iteration clojure/acronym

--- clojure/acronym/src/acronym.clj
+++ clojure/acronym/src/acronym.clj
@@ -1,16 +1,11 @@
 (ns acronym)
 (use '[clojure.string :as s])
 
-(defn map2 [l f] (map f l))
-(defn join2 [l s] (s/join s l))
-
 (defn acronym [phrase]
-    (-> phrase
+    (->> (-> phrase
         (s/replace #"-" " ")
         (s/replace #"HyperText" "Hyper Text")
-        (s/split #" ")
-        (map2 #(str (first %)))
-        (join2 "")
-        (s/upper-case)
-    )
-)
+        (s/split #" "))
+        (map #(str (first %)))
+        (s/join "")
+        (s/upper-case)))
