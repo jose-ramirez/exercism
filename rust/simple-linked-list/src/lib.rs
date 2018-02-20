47b0e4fbd7dc064204199fb73dba1f701974bc58
Jose Ramirez <a30673083@gmail.com>
2018-02-20 05:34:27 -0300

    Real implementation of a linked list in Rust

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -1,52 +1,82 @@
-#[derive(Default)]
 pub struct SimpleLinkedList<T> {
-    pub nodes: Vec<T>,
+    head: Option<Box<Node<T>>>,
+}
+
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        Self { nodes: vec![] }
+        SimpleLinkedList { head: None }
     }
 
     pub fn len(&self) -> usize {
-        self.nodes.len()
+        let mut total = 0;
+        let mut h = &self.head;
+        loop {
+            match *h {
+                None => break,
+                Some(ref v) => h = &v.next,
+            };
+            total += 1;
+        }
+        total
     }
 
     pub fn push(&mut self, element: T) {
-        self.nodes.push(element)
+        self.head = Some(Box::new(Node {
+            value: element,
+            next: self.head.take(),
+        }))
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.nodes.pop()
+        match self.head.take() {
+            None => None,
+            Some(mut val) => {
+                self.head = val.next.take();
+                Some(val.value)
+            }
+        }
     }
 
     pub fn peek(&self) -> Option<&T> {
-        self.nodes.last()
+        match self.head {
+            None => None,
+            Some(ref node) => Some(&node.value),
+        }
     }
 }
 
 impl<T: Clone> SimpleLinkedList<T> {
     pub fn rev(&self) -> SimpleLinkedList<T> {
-        let mut s = SimpleLinkedList::<T>::new();
-        for n in &self.nodes {
-            s.nodes.insert(0, n.clone());
+        let mut list = SimpleLinkedList::new();
+        let mut head = &self.head;
+        while let &Some(ref node) = head {
+            list.push(node.value.clone());
+            head = &node.next;
         }
-        s
+        list
     }
 }
 
 impl<'a, T: Clone> From<&'a [T]> for SimpleLinkedList<T> {
     fn from(item: &[T]) -> Self {
-        let mut s = SimpleLinkedList::<T>::new();
-        for i in item {
-            s.push(i.clone());
-        }
-        s
+        let mut l = SimpleLinkedList::new();
+        item.iter().for_each(|x| l.push(x.clone()));
+        l
     }
 }
 
 impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
     fn into(mut self) -> Vec<T> {
-        self.nodes
+        let mut v = Vec::new();
+        while let Some(n) = self.pop() {
+            v.push(n);
+        }
+        v.reverse();
+        v
     }
 }
